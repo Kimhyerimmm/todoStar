dca42a0b13897576a8aada139856112666eb776c
hyerim <ax1021@naver.com>
2024-08-28 14:27:39 +0900

    [프로젝트 기본 세팅] 프로젝트 파일 생성

---
+++ todoStar/todoStar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todoStar
+//
+//  Created by 김혜림 on 8/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
