149ea39d87533f07a5f1bede5e778ee3119d114d
hyerim <ax1021@naver.com>
2024-09-06 14:56:38 +0900

    [Goal List] importanceHighView, importanceMiddleView, importanceRowView 및 하위 뷰 생성 / 오토 레이아웃

--- todoStar/todoStar/Sources/Modules/Goal/GoalList/GoalListView.swift
+++ todoStar/todoStar/Sources/Modules/Goal/GoalList/GoalListView.swift
@@ -12,6 +12,21 @@ class GoalListView: UIView {
     // MARK: - Property
     private let viewModel = GoalListViewModel()
 
+    // 중요도 높음 리스트
+    private let importanceHighView = UIView()
+    private let highTitle = UILabel()
+    private let highTableView = UITableView()
+
+    // 중요도 중간 리스트
+    private let importanceMiddleView = UIView()
+    private let middleTitle = UILabel()
+    private let middleTableView = UITableView()
+
+    // 중요도 낮음 리스트
+    private let importanceRowView = UIView()
+    private let rowTitle = UILabel()
+    private let rowTableView = UITableView()
+
 
     // MARK: Life Cycle
     override init(frame: CGRect) {
@@ -26,9 +41,38 @@ class GoalListView: UIView {
     
     // MARK: - Setup Method
     func setupView() {
+        addSubview(importanceHighView)
+        addSubview(importanceMiddleView)
+        addSubview(importanceRowView)
 
+        importanceHighView.translatesAutoresizingMaskIntoConstraints = false
+        importanceMiddleView.translatesAutoresizingMaskIntoConstraints = false
+        importanceRowView.translatesAutoresizingMaskIntoConstraints = false
+
+        // importanceHighView 오토 레이아웃
+        NSLayoutConstraint.activate([
+            importanceHighView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 30),
+            importanceHighView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            importanceHighView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 20)
+        ])
+
+        // importanceMiddleView 오토 레이아웃
+        NSLayoutConstraint.activate([
+            importanceMiddleView.topAnchor.constraint(equalTo: importanceHighView.bottomAnchor, constant: 40),
+            importanceMiddleView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            importanceMiddleView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 20)
+        ])
+
+        // importanceRowView 오토 레이아웃
+        NSLayoutConstraint.activate([
+            importanceRowView.topAnchor.constraint(equalTo: importanceMiddleView.bottomAnchor, constant: 40),
+            importanceRowView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            importanceRowView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 20)
+        ])
     }
 
 
     // MARK: - View
+    // MARK: 중요도 높음 리스트
+    
 }
--- todoStar/todoStar/Sources/Modules/Goal/GoalList/GoalListViewController.swift
+++ todoStar/todoStar/Sources/Modules/Goal/GoalList/GoalListViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class GoalListViewController: UIViewController {
+class GoalListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     // MARK: - Property
     let goalListView = GoalListView()
@@ -51,6 +51,15 @@ class GoalListViewController: UIViewController {
 
 
     // MARK: - Data Source
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        return cell
+    }
 
 
     // MARK: - Delegate
